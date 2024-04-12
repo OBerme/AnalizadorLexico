3fb5956b7eca66a2310dc0d0c6ed019fdd0e531e
osbermej <oscarestudios2001@gmail.com>
2024-04-12 13:45:46 +0200

    Added the structure of the proyect and Tests.java

---
+++ src/test/java/automata/AnalizadorFM.java
@@ -0,0 +1,11 @@
+package automata;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+
+public class AnalizadorFM {
+    
+}
---
+++ src/test/java/automata/Tests.java
@@ -0,0 +1,8 @@
+package automata;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AnalizadorLexicoTest.class })
+public class Tests {}
